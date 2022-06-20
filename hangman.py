8c792950e87c297b3a24e86cbb341b33379a761a
ronkyma <ronkyonline88@yahoo.com>
2022-06-20 13:34:39 +0200

    adjusted hangman spelling game

--- hangman.py
+++ hangman.py
@@ -15,13 +15,15 @@ def hangman():
     alphabet = set(string.ascii_uppercase)
     used_letters = set() # what the user has guessed
 
+    lives = 6
+
     # getting user input
-    while len(word_letters) > 0:
+    while len(word_letters) > 0 and lives > 0:
         # letters used
         # ' '.join(['a', 'b', 'cd']) --> 'a b cd'
-        print('You have used these letters: ', ' '.join(used_letters))
+        print('You have', lives, 'lives left and you have used these letters: ', ' '.join(used_letters))
 
-         # what current word is (ie W - R D)
+        # what current word is (ie W - R D)
         word_list = [letter if letter in used_letters else '-' for letter in word]
         print('Current word: ', ' '.join(word_list))
 
@@ -31,10 +33,19 @@ def hangman():
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
     
+            else:
+                lives = lives -1 # takes away a life if wrong
+                print('Letter is not in word.')
+
         elif user_letter in used_letters:
             print('You have already used that character. Please try again')
 
         else:
             print('Invalid character. Please try again.')
+    # gets here whe len(word_letters) == 0 OR when lives == 0
+    if lives == 0:
+        print('You died, sorry. The word was', word)
+    else:
+        print('You guessed the word', word, '!!')
 
 hangman()
