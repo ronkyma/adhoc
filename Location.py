2248133b76628bd3ac9486e7788ce8b4cd31d930
ronkyma <ronkyonline88@yahoo.com>
2023-09-05 23:46:07 +0200

    Location-based analysis

---
+++ Location.py
@@ -0,0 +1,18 @@
+import geopandas as gpd
+
+# Load point data (e.g., addresses, GPS coordinates)
+points_gdf = gpd.read_file('points.shp')
+
+# Load polygon data (e.g., administrative boundaries)
+polygons_gdf = gpd.read_file('polygons.shp')
+
+# Spatially join points within polygons
+joined_gdf = gpd.sjoin(points_gdf, polygons_gdf, op='within')
+
+# Calculate statistics (e.g., count of points per polygon)
+stats = joined_gdf.groupby('polygon_attribute').size().reset_index(name='point_count')
+
+# Save the results to a new shapefile
+stats.to_file('analysis_results.shp')
+
+print("Analysis results saved to 'analysis_results.shp'.")
